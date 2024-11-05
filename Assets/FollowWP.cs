27f4c8a7e407a4d64e74ad6d640f8bd7b95e5ddd
Skrycer <ciceroserre@gmail.com>
2024-11-05 23:00:20 +0000

    Waipoints
    Following a Circuit&Tracker
    Using A* with Waypoints Part 1

---
+++ Assets/FollowWP.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+
+
+public class FollowWP : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int currentWP = 0;
+
+    public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp (this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
